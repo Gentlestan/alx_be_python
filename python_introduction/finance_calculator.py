30ef041687c1d0cfade45b1782f98e52b680bf2c
Ohazulike Stanley <sohazulike@gmail.com>
2025-05-21 03:55:57 +1000

    corrected formular on finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -5,7 +5,7 @@ user_monthly_saving = user_monthly_income - user_total_monthly_expenses
 
 simple_annual_interest = 0.05
 
-projected_savings = user_monthly_income * 12 + (user_monthly_saving * 12 * simple_annual_interest) #this is projected annual saving formulae
+projected_savings = user_monthly_saving * 12 + (user_monthly_saving * 12 * simple_annual_interest) #this is projected annual saving formulae
 
 print("Your monthly savings are", "$" + str(user_monthly_saving))
 print("projected savings after one year, with interest is:", "$" + str(projected_savings))
