6dc23ebf9d72659528a8a1c9abe9f266d8a6768a
Ohazulike Stanley <sohazulike@gmail.com>
2025-05-20 23:50:20 +1000

    corrected the simple interest formular

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,7 +1,7 @@
-P = 1000
-R = 0.05
-T = 3
+Principal = 1000
+Rate = 0.05
+Time = 3
 
-I = P * R * T
+Interest = Principal * Rate * Time
 
-print("The simple interest is:", I)
+print("The simple interest is:", Interest)
